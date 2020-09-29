8cfaa7b034ce3b61ef621af4382a0b380186c0a0
viktor.bugaevskiy <viktor.bugaevskiy@x5.ru>
2020-09-29 21:03:54 +0300

    add

--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -12,23 +12,23 @@ export class SearchPipe implements PipeTransform {
     return value.filter((val) => {
       let rVal;
       if (args[0] != "") {
-        rVal = (val.name.toLocaleLowerCase().includes(args[0]));
+        rVal = (val.name.includes(args[0]));
         if (!rVal) {
-          rVal = (val.username.toLocaleLowerCase().includes(args[0]));
+          rVal = (val.username.includes(args[0]));
         }
         return rVal;
       } else if (args[1] != "") {
-        return rVal = (val.email.toLocaleLowerCase().includes(args[1]));
+        return rVal = (val.email.includes(args[1]));
       } else if (args[2] != "") {
-        rVal = (val.address.street.toLocaleLowerCase().includes(args[2]));
+        rVal = (val.address.street.includes(args[2]));
         if (!rVal) {
-          rVal = (val.address.suite.toLocaleLowerCase().includes(args[2]));
+          rVal = (val.address.suite.includes(args[2]));
         } 
         if (!rVal) {
-          rVal = (val.address.city.toLocaleLowerCase().includes(args[2]));
+          rVal = (val.address.city.includes(args[2]));
         }
          if (!rVal) {
-          rVal = (val.address.zipcode.toLocaleLowerCase().includes(args[2]));
+          rVal = (val.address.zipcode.includes(args[2]));
         }
         return rVal;
       } else{
